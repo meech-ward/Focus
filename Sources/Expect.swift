dcf02318f10f63d6cd0193298d0d17df07e934bc
Sam Meech-Ward <sam@meech-ward.com>
2016-12-19 08:58:52 -0600

    Create an Expectable protocol that defines what the Expect object needs to implement

--- Sources/Expect.swift
+++ Sources/Expect.swift
@@ -8,14 +8,14 @@
 
 import Foundation
 
-public struct Expect<Item> {
+public struct Expect<Item>: Expectable {
     
     public var to: To<Item> {
         return _to
     }
     private let _to: To<Item>!
     
-    init(item: Item) {
+    public init(item: Item) {
         _to = To(item: item)
     }
 }
