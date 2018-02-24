ec883921af061bba4395346a27be4c8c4f3b9bf6
Sam Meech-Ward <sam@meech-ward.com>
2018-02-24 14:57:08 -0800

    Add an cocoa touch framework for the source files

---
+++ iOS/Focus/Focus.h
@@ -0,0 +1,19 @@
+//
+//  Focus.h
+//  Focus
+//
+//  Created by Sam Meech-Ward on 2018-02-24.
+//  Copyright © 2018 meech-ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Focus.
+FOUNDATION_EXPORT double FocusVersionNumber;
+
+//! Project version string for Focus.
+FOUNDATION_EXPORT const unsigned char FocusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Focus/PublicHeader.h>
+
+
